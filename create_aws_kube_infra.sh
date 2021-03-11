9679a29d22718afaefbdb83eae55b2ad2d554e66
Divya <divya.udacity@gmail.com>
2021-03-11 11:06:19 +0400

    Deploy Kube Cluster

--- create_aws_kube_infra.sh
+++ create_aws_kube_infra.sh
@@ -4,5 +4,5 @@ if [[ $count -gt 0 ]]
     echo "Skipping Cluster creation .. Infrastructure Exists"
     else
     echo "Creatiing AWS Kubernetes cluster"
-    eksctl create cluster --name capstone-divya --version 1.19 --region $AWS_DEFAULT_REGION --nodegroup-name linux-nodes --nodes 3  --nodes-min 1 --nodes-max 4 --managed
+    eksctl create cluster --name capstone-divya --version 1.19 --region $AWS_DEFAULT_REGION --nodegroup-name capstone-divya --node-type t3.large --nodes-min 2 --nodes-max 4 --managed
 fi
